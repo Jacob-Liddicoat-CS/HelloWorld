52c7902a34b3bfa88dbcf5503bb45efce8e03034
Jacob Liddicoat <j.liddicoat@share.epsb.ca>
2018-12-14 11:30:39 -0700

    Added Comments

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,35 @@
+/**<h1>Project Title: Hello World</h1>
+*<b>Purpose of Project:</b><br>
+*<p>To create a folder, interact with CMD, interact with WYSIWYG</p>
+*<b>How to start the project instructions:</b><br>
+*<p>In CMD: $javac [ClassName].java; then $ java [CalssName]</p>
+*<b>User Instructions:<b/><br>
+*<p>Familiarize with routines of executing JAVA from CMD</P>
+*<br>
+*@author Jacob Liddicoat
+*@version 1
+*@since 2018-12-14
+*/
+
 public class HelloWorld
 {
-  public static void main (String[] args)
-  {
-    System.out.println ("Hello World");
-  }
+  /**<b>Class Description: Beginning Template</b><br>
+  *<p>Outputs message</p>
+  *@param args unused
+  *@return void
+  *@throws
+  */
+
+  /**<b>Method Description: Driver</b><br>
+  *<p>Outputs message</p>
+  *@param args unused
+  *@return void
+  *@throws
+  */
+
+   public static void main(String[] args)
+   {
+      System.out.println("Hello World!");
+   }
+
 }
